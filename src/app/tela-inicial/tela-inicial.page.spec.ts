f9dd0aef96d74dc616308a22e2bc407d3b412b6e
Yuri <86446634+YuriiSouza@users.noreply.github.com>
2023-06-09 14:14:38 -0300

    config cores e tela inicial

---
+++ src/app/tela-inicial/tela-inicial.page.spec.ts
@@ -0,0 +1,19 @@
+import { ComponentFixture, TestBed, async } from '@angular/core/testing';
+import { TelaInicialPage } from './tela-inicial.page';
+
+
+
+describe('TelaInicialPage', () => {
+  let component: TelaInicialPage;
+  let fixture: ComponentFixture<TelaInicialPage>;
+
+  beforeEach(async(() => {
+    fixture = TestBed.createComponent(TelaInicialPage);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  }));
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
