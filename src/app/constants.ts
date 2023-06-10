57f2f12a4e62edb65dd9543f7842f64b8707b79a
Vinicius Melo <viniciumelo@hotmail.com>
2023-06-10 10:31:33 -0300

    update

---
+++ src/app/constants.ts
@@ -0,0 +1,1 @@
+export const OPENAI_API_KEY = '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';
--- src/app/manual/manual.page.ts
+++ src/app/manual/manual.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import axios from 'axios';
+import { OPENAI_API_KEY } from '@constants/constants';
 
 @Component({
   selector: 'app-tab1',
@@ -6,7 +8,68 @@ import { Component } from '@angular/core';
   styleUrls: ['manual.page.scss']
 })
 export class ManualPage {
+  private isRequestPending = false;
+  private requestQueue: (() => void)[] = [];
 
   constructor() {}
 
+  async fazerPergunta() {
+    if (this.isRequestPending) {
+      this.requestQueue.push(() => this.fazerPergunta());
+      return;
+    }
+
+    const pergunta = "O que é TOD?";
+    try {
+      this.isRequestPending = true;
+      const resposta = await this.enviarPerguntaAoChatbot(pergunta);
+      console.log(resposta);
+      // You can display the response on the screen by assigning it to a variable or showing it in an alert, for example.
+    } catch (error: any) {
+      console.error(error);
+      if (error.response && error.response.status === 429) {
+        // Retry the request after a delay
+        await this.delay(2000); // Adjust the delay time as needed
+        await this.fazerPergunta();
+      }
+    } finally {
+      this.isRequestPending = false;
+      if (this.requestQueue.length > 0) {
+        const nextRequest = this.requestQueue.shift();
+        if (nextRequest) {
+          nextRequest();
+        }
+      }
+    }
+  }
+
+  async enviarPerguntaAoChatbot(pergunta: string) {
+    const prompt = pergunta;
+    const maxTokens = 50;
+    const n = 1;
+    const stop = '\n';
+
+    const headers = {
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${OPENAI_API_KEY}`
+    };
+
+    const data = {
+      prompt,
+      max_tokens: maxTokens,
+      n,
+      stop
+    };
+
+    try {
+      const response = await axios.post('https://api.openai.com/v1/engines/text-davinci-003/completions', data, { headers });
+      return response.data.choices[0].text.trim();
+    } catch (error: any) {
+      throw new Error(`Request failed with status code ${error.response.status}`);
+    }
+  }
+
+  delay(ms: number) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
 }
