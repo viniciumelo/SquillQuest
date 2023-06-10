4b0af8829a954fae7c99f7be09de561ea5d90e3c
Vinicius Melo <viniciumelo@hotmail.com>
2023-06-10 00:02:04 -0300

    update

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { NavController } from '@ionic/angular';
+import { CreateAccountPage } from '../create-account/create-account.page';
 
 @Component({
   selector: 'app-login',
@@ -9,9 +11,13 @@ export class LoginPage implements OnInit {
   
 
   
-  constructor() { }
+  constructor(private navCtrl: NavController) { }
 
   ngOnInit() {
+  }
+
+  goToCreateAccount() {
+    this.navCtrl.navigateForward('/create-account');
   }
 
 }
